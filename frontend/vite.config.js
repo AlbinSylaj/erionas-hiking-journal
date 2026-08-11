a22e04406fcbb4d99b7fea556787f8ce7b94051b
Albin Sylaj <albinsylaj31@gmail.com>
2026-08-11 23:16:39 +0200

    Use root assets for custom domain

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,9 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-const repositoryName = process.env.GITHUB_REPOSITORY?.split('/')[1] || 'erionas-hiking-journal'
-
 export default defineConfig({
-  base: process.env.GITHUB_ACTIONS ? `/${repositoryName}/` : '/',
+  base: '/',
   plugins: [react()],
 })
